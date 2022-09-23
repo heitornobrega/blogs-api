db7495aa1af94b25a052418d86bba0e054188099
heitornobrega <nobheitor@gmail.com>
2022-09-23 17:11:51 -0300

    req 02

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+      },
+  },
+  { timestamps: false, underscored: true, tableName: "users" }
+  );
+    return User;
+};
